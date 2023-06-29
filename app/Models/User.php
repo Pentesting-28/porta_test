7a18e491b89a0251ff6756510bed7b5133a9f7d3
Pentesting-28 <mpenav28@gmail.com>
2023-06-29 14:15:40 -0400

    Se agregaron 3 campos a la asignación masiva del modelo de la tabla users.

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone_number',
+        'profile_photo_path',
+        'last_login'
     ];
 
     /**
