98954b68f862b67d3f02442445518aa90675d7cb
Pentesting-28 <mpenav28@gmail.com>
2023-06-29 14:47:10 -0400

    Creacion de la view register

--- app/Http/Controllers/Web/Auth/AuthController.php
+++ app/Http/Controllers/Web/Auth/AuthController.php
@@ -10,4 +10,9 @@ class AuthController extends Controller
     public function loginView() {
         return view('auth.login');
     }
+
+    public function registerView()
+    {
+        return view('auth.register');
+    }
 }
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,78 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="row justify-content-center mt-5">
+    <div class="col-md-8">
+
+        <div class="card">
+            <div class="card-header">Register</div>
+            <div class="card-body">
+                <form action="" method="post" enctype="multipart/form-data">
+                    @csrf
+                    <div class="mb-3 row">
+                        <label for="name" class="col-md-4 col-form-label text-md-end text-start">Nombre</label>
+                        <div class="col-md-6">
+                          <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name') }}">
+                            @if ($errors->has('name'))
+                                <span class="text-danger">{{ $errors->first('name') }}</span>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="email" class="col-md-4 col-form-label text-md-end text-start">Dirección de correo</label>
+                        <div class="col-md-6">
+                          <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email') }}">
+                            @if ($errors->has('email'))
+                                <span class="text-danger">{{ $errors->first('email') }}</span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="mb-3 row">
+                        <label for="phone_number" class="col-md-4 col-form-label text-md-end text-start">Número de teléfono</label>
+                        <div class="col-md-6">
+                          <input type="text" class="form-control @error('phone_number') is-invalid @enderror" id="phone_number" name="phone_number" value="{{ old('email') }}">
+                            @if ($errors->has('phone_number'))
+                                <span class="text-danger">{{ $errors->first('phone_number') }}</span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="mb-3 row">
+                        <label for="profile_photo" class="col-md-4 col-form-label text-md-end text-start">Foto de Perfil</label>
+                        <div class="col-md-6">
+                          <input type="file" class="form-control @error('profile_photo') is-invalid @enderror" id="profile_photo" name="profile_photo" value="{{ old('email') }}">
+                            @if ($errors->has('profile_photo'))
+                                <span class="text-danger">{{ $errors->first('profile_photo') }}</span>
+                            @endif
+                        </div>
+                    </div>
+
+
+                    <div class="mb-3 row">
+                        <label for="password" class="col-md-4 col-form-label text-md-end text-start">Contraseña</label>
+                        <div class="col-md-6">
+                          <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password">
+                            @if ($errors->has('password'))
+                                <span class="text-danger">{{ $errors->first('password') }}</span>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="password_confirmation" class="col-md-4 col-form-label text-md-end text-start">Confirmación de contraseña</label>
+                        <div class="col-md-6">
+                          <input type="password" class="form-control" id="password_confirmation" name="password_confirmation">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <input type="submit" class="col-md-3 offset-md-5 btn btn-primary" value="Registrar">
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
