5c9d672b258893937bc2c35cfd53fd3932c771c1
Pentesting-28 <mpenav28@gmail.com>
2023-06-29 14:51:27 -0400

    Se habilitaron el acceso a las rutas para ingresar a la view de register y login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,10 +19,10 @@
             <ul class="navbar-nav ms-auto">
                 @guest
                     <li class="nav-item">
-                        <a class="nav-link {{-- (request()->is('login')) ? 'active' : '' --}}" href="{{-- route('loginView') --}}">Login</a>
+                        <a class="nav-link {{ (request()->is('login')) ? 'active' : '' }}" href="{{ route('loginView') }}">Login</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link {{-- (request()->is('register')) ? 'active' : '' --}}" href="{{-- route('registerView') --}}">Registrar</a>
+                        <a class="nav-link {{ (request()->is('register')) ? 'active' : '' }}" href="{{ route('registerView') }}">Registrar</a>
                     </li>
                 @else
                     <li class="nav-item dropdown">
